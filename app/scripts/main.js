f5cb48511f2ad1dfbed340b49c9e1240f3f4b9b1
D Paul Childs <clausewitzian@gmail.com>
2019-08-29 14:46:03 -0400

    intro page complete

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,21 +2,62 @@
 
 Questions:
 
-Animation perspective issue on card fip.
-
-Animation card drop issue on card flip.
-
-Animation issue on step2 re-applying after card is flipped back to question side. I can't remove the step2 class when it arrives (I don't think I can), how can I make it stay in place until time to get rid of it?
-
-Animation issue on "getting rid" of a card that is in answer flipped state. Maybe flip it back automatically is the simplest fix.
-
-
-Work on 3d.
-Move click event listener to the card sides, not the container.
-Branch.
+Work on 3d. Done.
+Move click event listener to the card sides, not the container. Done.
+Branch. Done. 
 Intro page.
 Create decks?
+
+Remove spam-ability.
+
+For intro page,
+
+Offer X premade deck, and immediately hide overlay if selected.
+
+Otherwise, inputs and
+
 */
+
+// Adding intro page
+const introPage = document.querySelector('.overlay');
+const introContainer = document.createElement('section')
+introContainer.classList.add('intro-container')
+const introDescrip = document.createElement('p')
+introDescrip.classList.add('intro-descrip')
+introDescrip.innerHTML = 'Create flashcards for your own study deck, or select from available options.'
+const introQuestion = document.createElement('input')
+introQuestion.classList.add('intro-question')
+introQuestion.placeholder = 'Write your question here...'
+const introAnswer = document.createElement('input')
+introAnswer.classList.add('intro-answer')
+introAnswer.placeholder = '... then write the answer here and click submit!'
+const introSubmit = document.createElement('span')
+introSubmit.classList.add('intro-submit')
+introSubmit.innerHTML = 'Submit'
+const introComplete = document.createElement('span')
+introComplete.classList.add('intro-complete')
+introComplete.innerHTML = 'Ready, Go'
+const availableOptions = document.createElement('div')
+availableOptions.classList.add('available-options')
+
+const jsStudyButton = document.createElement('span')
+jsStudyButton.classList.add('js-study-button')
+jsStudyButton.innerHTML = 'JavaScript'
+jsStudyButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    introPage.classList.add('intro-fade-add')
+    // introPage.style.display = 'none'
+})
+
+availableOptions.appendChild(jsStudyButton)
+
+introPage.appendChild(availableOptions)
+introContainer.appendChild(introComplete)
+introContainer.appendChild(introSubmit)
+introContainer.appendChild(introAnswer)
+introContainer.appendChild(introQuestion)
+introContainer.appendChild(introDescrip)
+introPage.appendChild(introContainer)
 
 
 
@@ -30,7 +71,7 @@ const cardArrayOfObj = [
         answer: 'Boolean, Number, String, Undefined, Null, Symbol'
     },
     {
-        question: 'T or F: forEach() returns a value to its array',
+        question: 'T or F: forEach() returns a value',
         answer: 'False'
     },
     {
@@ -89,99 +130,94 @@ function createBoard() {
     } 
     
     
-
-    // Questions:
-    /*
-        What is the relationship of absolute position to normal display positions? (Why are the buttons bouncing down and back up?)
-
-    */
-
        
 
-        // New loop to iterate through unanswered questions and create cards to send to the DOM.
-        // Call new card.
-        function newCard() {
-             
-            //Take one card from unanswered array and create it on the DOM
-            let currentCard = unansweredArray[0]
+    // New loop to iterate through unanswered questions and create cards to send to the DOM.
+    // Call new card.
+    function newCard() {
             
-            //Outer box of each card that allows arrangement on the DOM
-            let outerBox = document.createElement('div');
-                outerBox.classList.add('card-control-initial-load')
+        //Take one card from unanswered array and create it on the DOM
+        let currentCard = unansweredArray[0]
+        
+        //Outer box of each card that allows arrangement on the DOM
+        let outerBox = document.createElement('div');
+            outerBox.classList.add('card-control-initial-load')
 
 
-                // Event listener for spacebar to flip cards
-                document.addEventListener('keypress', (evt) => {
-                    evt.preventDefault();
-                    if(evt.code === 'Space') {
-                        innerBox.classList.toggle('flipped')
-                    }
-                } )
-
-                // outerBox.setAttribute('card-number', randomArray[i]);
-                outerBox.classList.add('step2');
-            
-            //Box of nested divs necessary to animate cards
-            let innerBox = document.createElement('div');
-                innerBox.classList.add('card');
-                innerBox.addEventListener('click', () => {
+            // Event listener for spacebar to flip cards
+            document.addEventListener('keypress', (evt) => {
+                evt.preventDefault();
+                if(evt.code === 'Space') {
                     innerBox.classList.toggle('flipped')
-                    console.log(innerBox.classList)
-                    // let innerBoxClasses = Array.from(innerBox.classList)
-                    // if(innerBoxClasses.some((el) => el === 'flipped')) {
-                    //     innerBox.classList.add('flipped2')
-                    // } else {
-                    //     innerBox.classList.remove('flipped2')
-                    // }
-                })
+                }
+            } )
 
-            //These two divs append as front and back to the above nest box    
-            let cardSide1 = document.createElement('div');
-                cardSide1.classList.add('card-answer');
-                cardSide1.innerHTML = currentCard.answer
-                
-            let cardSide2 = document.createElement('div');
-                cardSide2.classList.add('card-question');
-                cardSide2.innerHTML = currentCard.question
+            // outerBox.setAttribute('card-number', randomArray[i]);
+            outerBox.classList.add('step2');
+        
+        //Box of nested divs necessary to animate cards
+        let innerBox = document.createElement('div');
+            innerBox.classList.add('card');
+            innerBox.addEventListener('click', () => {
+                innerBox.classList.toggle('flipped')
+                console.log(innerBox.classList)
+                // let innerBoxClasses = Array.from(innerBox.classList)
+                // if(innerBoxClasses.some((el) => el === 'flipped')) {
+                //     innerBox.classList.add('flipped2')
+                // } else {
+                //     innerBox.classList.remove('flipped2')
+                // }
+            })
 
-                innerBox.appendChild(cardSide1);
-                innerBox.appendChild(cardSide2);
-                
-                outerBox.appendChild(innerBox);
-                mainBox.appendChild(outerBox);
-
+        //These two divs append as front and back to the above nest box    
+        let cardSide1 = document.createElement('div');
+            cardSide1.classList.add('card-answer');
+            cardSide1.innerHTML = currentCard.answer
             
+        let cardSide2 = document.createElement('div');
+            cardSide2.classList.add('card-question');
+            cardSide2.innerHTML = currentCard.question
 
-            // Grab list buttons
-            let nextCardButton = document.querySelector('.next-card')
-            let correctAnswerButton = document.querySelector('.correct-answer')
+            innerBox.appendChild(cardSide1);
+            innerBox.appendChild(cardSide2);
+            
+            outerBox.appendChild(innerBox);
+            mainBox.appendChild(outerBox);
 
-            nextCardButton.addEventListener('click', getNextCard)
-            correctAnswerButton.addEventListener('click', rightAnswer)
-                
-            function getNextCard(evt) {
-                evt.preventDefault();
-                outerBox.classList.add('step3')
-                let firstEl = unansweredArray.shift()
-                unansweredArray.push(firstEl)
-                console.log(outerBox.parentNode.childNodes) // WTF, why does console.log fix the double card problem?
-                setTimeout(() => outerBox.parentNode.removeChild(outerBox), 600)
-                setTimeout(() => newCard(), 600); // The delay must be exactly the same or else the DOM tangles up.
-            }
-            function rightAnswer(evt) {
-                evt.preventDefault();
-                // call getNextCard, remove the current card from unanswered array
-                outerBox.classList.add('step3')
-                console.log(outerBox.parentNode.childNodes) // WTF, why does console.log fix the double card problem?
-                setTimeout(() => outerBox.parentNode.removeChild(outerBox), 600)
-                unansweredArray.splice(0, 1)
-                setTimeout(() => newCard(), 600); // The delay must be exactly the same or else the DOM tangles up.
+        
 
-            }
+        // Grab list buttons
+        let nextCardButton = document.querySelector('.next-card')
+        let correctAnswerButton = document.querySelector('.correct-answer')
 
-        } // This closes newCard()
+        nextCardButton.addEventListener('click', getNextCard)
+        correctAnswerButton.addEventListener('click', rightAnswer)
+            
+        function getNextCard(evt) {
+            evt.preventDefault();
+            outerBox.classList.add('step3')
+            innerBox.classList.toggle('flipped')
+            let firstEl = unansweredArray.shift()
+            unansweredArray.push(firstEl)
+            console.log(outerBox.parentNode.childNodes) // WTF, why does console.log fix the double card problem?
+            setTimeout(() => outerBox.parentNode.removeChild(outerBox), 600)
+            setTimeout(() => newCard(), 600); // The delay must be exactly the same or else the DOM tangles up.
+        }
+        function rightAnswer(evt) {
+            evt.preventDefault();
+            // call getNextCard, remove the current card from unanswered array
+            outerBox.classList.add('step3')
+            innerBox.classList.toggle('flipped')
+            console.log(outerBox.parentNode.childNodes) // WTF, why does console.log fix the double card problem?
+            setTimeout(() => outerBox.parentNode.removeChild(outerBox), 600)
+            unansweredArray.splice(0, 1)
+            setTimeout(() => newCard(), 600); // The delay must be exactly the same or else the DOM tangles up.
 
-        newCard();
+        }
+
+    } // This closes newCard()
+
+    newCard();
 
 } // This closes createBoard()
     
